a68e68ed3d3273827fcdad3755b723afcbf94ab3
akeeste <akeeste@sandia.gov>
2026-04-30 16:02:44 -0500

    working but non differentiable cic example, functions. SS functions

--- src/Bemio.jl
+++ src/Bemio.jl
@@ -3,6 +3,11 @@ module Bemio
 import NCDatasets
 import NetCDF
 import Integrals
+using Statistics
+import LinearAlgebra
+import ToeplitzMatrices
+using ExponentialUtilities
+import Unitful
 
 struct Hydro
     radiating_dof::Any
@@ -58,9 +63,112 @@ function read_capytaine(filename::String)::Hydro
         wavelength, depth, volume, cb, ex, fk, di, am, rd, khs)
 end
 
-function radiationIRF!(myHydro::Hydro, timeEnd = 60, nDt::Int = 1001, nDw::Int = 1001,
-        wMin = minimum(myHydro.w), wMax = maximum(myHydro.w))
-    time = collect(0:(timeEnd / (nDt - 1)):timeEnd)
+function radiation_irf(
+        rd_raw::Array, w_raw::Vector; w_max::Float64 = 20.0, t_f = 60, dt::Float64 = 0.01)
+    # cut off at the frequency limit
+    i_w_end = argmin(abs.(w_raw .- w_max))
+    w = w_raw[1:i_w_end]
+    rd = rd_raw[:, :, 1:i_w_end]
+    nw = length(w)
+
+    # timeseries array
+    t = collect(0:dt:t_f)'
+    nt = length(t)
+
+    # Reshape arrays to enable element-wise multiplication without loops and overwriting initialized arrays
+    c = reshape(cos.(w * t), 1, 1, nw, nt)
+    dw = reshape(diff(w), 1, 1, nw - 1)
+    integrand = rd .* c
+    integral = sum(
+        (integrand[:, :, 1:(end - 1), :] .+ integrand[:, :, 2:end, :]) .* 0.5 .* dw;
+        dims = [3])[:, :, 1, :]
+    Kᵣ = 2 / pi .* integral
+    tᵣ = reshape(t, 1, 1, size(Kᵣ, 3))
+    Kᵣ, tᵣ
+end
+
+function radiation_state_space(Kᵣ, tᵣ, max_order = 10, R2t = 0.95)
+    dt = Unitful.ustrip.(tᵣ[2] - tᵣ[1])
+
+    ss_A = zeros(size(Kᵣ, 1), size(Kᵣ, 2), max_order, max_order)
+    ss_B = zeros(size(Kᵣ, 1), size(Kᵣ, 2), max_order, 1)
+    ss_C = zeros(size(Kᵣ, 1), size(Kᵣ, 2), 1, max_order)
+    ss_D = zeros(size(Kᵣ)[1:2])
+    ss_K = zeros(size(Kᵣ)[1:3])
+    ss_R2 = zeros(size(Kᵣ)[1:2])
+    ss_order = zeros(size(Kᵣ)[1:2])
+
+    for i in axes(Kᵣ)[1], j in axes(Kᵣ)[2]
+        irf_K = Unitful.ustrip.(Kᵣ[i, j, :])
+        R2i = LinearAlgebra.norm(irf_K .- mean(irf_K))
+        R2 = R2i
+
+        order = 1 # Initial state space order
+        y = dt .* irf_K
+        n = length(y)
+        h = ToeplitzMatrices.Hankel([y[2:end]; zeros(n - 1)], (n - 1, n - 1))
+        u, svh, v = LinearAlgebra.svd(h)
+
+        # Define variables so they pass outside of the loop 
+        local ac, bc, cc, dc, ss_K, R2
+        while R2 > R2t && order <= max_order
+            order += 1
+
+            u1 = u[1:(n - 2), 1:order]
+            v1 = v[1:(n - 2), 1:order]
+            u2 = u[2:(n - 1), 1:order]
+            sqs = sqrt.(svh[1:order])
+            ubar = u1' * u2
+
+            a = ubar .* ((1 ./ sqs) * sqs')
+            b = v1[1, :] .* sqs
+            c = (u1[1, :] .* sqs)'
+            d = y[1]
+
+            iidd = inv(dt / 2 * (LinearAlgebra.I(order) + a))  # (T/2*I+T/2*A)^{-1} = 2/T(I+A)^{-1}
+            ac = (a - LinearAlgebra.I(order)) * iidd           # (A-I)2/T(I+A)^{-1} = 2/T(A-I)(I+A)^{-1}
+            bc = dt * (iidd * b)                               # (T/2+T/2)*2/T(I+A)^{-1}B = 2(I+A)^{-1}B
+            cc = c * iidd                                      # C*2/T(I+A)^{-1} = 2/T(I+A)^{-1}
+            dc = d .- dt / 2 * ((c * iidd) * b)                # D-T/2C (2/T(I+A)^{-1})B = D-C(I+A)^{-1})B
+
+            ss_K = zeros(length(tᵣ))
+            method = ExpMethodNative()
+            term = ac * dt * 0
+            cache = ExponentialUtilities.alloc_mem(term, method) # Main allocation done here
+            for k in 1:length(tᵣ)
+                term = ac * dt * (k - 1)
+                exponential!(term) # Very little allocation here
+                ss_K[k] = ((cc * term) * bc)[1] # IRF approximation using SS result
+            end
+
+            # Notes: It is unclear why R2 is scaled by R2i (the initial R2, whose definition seems dubious) before being compared to the
+            # 5% threshold. This is what WEC-Sim does and may be why the insignificant coupled dofs get forced to such high orders
+            R2 = 1 - (LinearAlgebra.norm(irf_K - ss_K) / R2i)^2
+        end
+        ss_A[i, j, 1:order, 1:order] = ac
+        ss_B[i, j, 1:order, 1] = bc
+        ss_C[i, j, 1, 1:order] = cc
+        ss_D[i, j] = dc
+        ss_K[i, j, :] = ss_K
+        ss_R2[i, j] = R2
+        ss_order[i, j] = order
+    end
+    return ss_A, ss_B, ss_C, ss_D, ss_K, ss_R2, ss_order
+end
+
+function alternate_Ainf(Kᵣ, A, w_raw, tCIC_raw)
+    nw = length(w_raw)
+    nt = length(tCIC_raw)
+    w = reshape(w_raw, 1, 1, 1, nw)
+    tCIC = reshape(tCIC_raw, 1, 1, nt)
+
+    integrand = Kᵣ .* sin.(w .* tCIC) # nDOF, nDOF, nt, nw
+    dt = diff(tCIC; dims = 3)
+    integral = sum(
+        (integrand[:, :, 1:(end - 1), :] .+ integrand[:, :, 2:end, :]) .* 0.5 .* dt;
+        dims = [3]) # nDOF, nDOF, 1, nw
+    ainf_temp = A + (integral ./ w)[:, :, 1, :] # nDOF, nDOF, nw
+    ainf = mean(ainf_temp; dims = [3])[:, :, 1] # nDOF, nDOF
 end
 
 end
--- src/core.jl
+++ src/core.jl
@@ -1,5 +1,8 @@
 import OrdinaryDiffEq as ODE
 import SciMLSensitivity as SMS # required for Zygote's reverse AD
+using DSP
+
+global velocity_history
 
 function ramp_function(start_time, ramp_time, current_time)
     if current_time < start_time
@@ -33,19 +36,73 @@ end
 function hydrodynamic_oscillator(du, u, p, t)
     (k, c, inverse_mass, excitation_coeff, constant_forces, wave) = p
     excitation_force = calculate_excitation_force(t, excitation_coeff, wave)
-    return inverse_mass * (-c * du - k * u + excitation_force + constant_forces)
+    return inverse_mass * (-c * du - k * u + excitation_force + constant_forces) # ddu
 end
 
-function hydrodynamic_oscillator_convolution(du, u, p, t)
-    (k, c, inverse_mass, excitation_coeff, constant_forces, wave) = p
+function hydrodynamic_oscillator_cic(du, u, p, t)
+    (k, c, inverse_mass, excitation_coeff, constant_forces, wave, cic) = p
+
+    # Convolution integral
+    Kᵣ, tᵣ = cic
+    global velocity_history = circshift(velocity_history, 1)
+    global velocity_history[1, :, 1] = du
+    # velocity_history = zeros(1, size(Kᵣ, 2), size(Kᵣ, 3))
+    # integrand = DSP.conv(Kᵣ, velocity_history) # nDOF, nDOF, nt
+    integrand = Kᵣ .* velocity_history # nDOF, nDOF, nt
+    dt = diff(tᵣ; dims = 3) # 1, 1, nt-1
+    rad_force = sum(
+        (integrand[:, :, 1:(end - 1)] .+ integrand[:, :, 2:end]) .* 0.5 .* dt;
+        dims = [3]) # nDOF, nDOF
+
     excitation_force = calculate_excitation_force(t, excitation_coeff, wave)
-    return inverse_mass * (-c * du - k * u + excitation_force + constant_forces)
+    return inverse_mass * (-c * du - k * u + excitation_force + constant_forces) # ddu
+end
+
+function hydrodynamic_oscillator_ss(du, u, p, t)
+    # added mass should utilize infinite frequency added mass only
+    # c should not include radiation damping
+    # system of equations in u and du should include velocity and the state space vector
+    (k, c, inverse_mass, excitation_coeff, constant_forces, wave, state_space) = p
+
+    # d(SS vector) = dx = Ar * x + Br * velocity
+    #    CI kernel =  y = Cr * x + Dr * velocity
+    # rad_force         = Cr * u[7:12] + Dr * du[1:6]
+    nU = length(u)
+    i_mid = Int(nU / 2)
+    if nU % 2 != 0
+        error("Hydrodynamics.jl:hydrodynamic_oscillator_ss:Length of ODEs is not even. " +
+              "Must have one state space ODE for each response ODE.")
+    end
+    Aᵣ, Bᵣ, Cᵣ, Dᵣ = state_space
+    rad_force = Cᵣ * u[(i_mid + 1):end] + Dᵣ * du[1:i_mid]
+    du_ss = Aᵣ * u[(i_mid + 1):end] + Br * du[1:i_mid]
+
+    excitation_force = calculate_excitation_force(t, excitation_coeff, wave)
+    return inverse_mass * (-c * du - k * u + excitation_force + constant_forces + rad_force) # ddu
 end
 
 function hydrodynamic_solver(dx₀, x₀, ts, p)
     dt = diff(ts[1:2])[1]
     ode_prob = ODE.SecondOrderODEProblem(
         hydrodynamic_oscillator, dx₀, x₀, ts[[1, end]], p)
+    ode_sol = ODE.solve(ode_prob, ODE.Vern6(), saveat = dt)
+    return ode_sol
+end
+
+function hydrodynamic_solver_cic(dx₀, x₀, ts, p)
+    dt = diff(ts[1:2])[1]
+
+    global velocity_history = zeros(1, size(p[7][1], 2), size(p[7][1], 3))
+    ode_prob = ODE.SecondOrderODEProblem(
+        hydrodynamic_oscillator_cic, dx₀, x₀, ts[[1, end]], p)
+    ode_sol = ODE.solve(ode_prob, ODE.Vern6(), saveat = dt)
+    return ode_sol
+end
+
+function hydrodynamic_solver_ss(dx₀, x₀, ts, p)
+    dt = diff(ts[1:2])[1]
+    ode_prob = ODE.SecondOrderODEProblem(
+        hydrodynamic_oscillator_ss, dx₀, x₀, ts[[1, end]], p)
     ode_sol = ODE.solve(ode_prob, ODE.Vern6(), saveat = dt)
     return ode_sol
 end
