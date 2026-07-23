fc14daa24d1886167eba6bcbe48f6f5c03ef0583
jtgrasb <87095491+jtgrasb@users.noreply.github.com>
2026-07-23 12:16:34 -0600

    add extra non-hydrodynamic states to oscillator

--- src/core.jl
+++ src/core.jl
@@ -1,7 +1,19 @@
 import OrdinaryDiffEq as ODE
 import SimpleDiffEq as SDE
 
-struct HydrodynamicSolution{TT, TX, TV}
+Base.@kwdef struct HydroParams
+    inverse_mass
+    hydro
+    force_other
+    u_control = nothing
+    p_other = nothing
+
+    extra_rhs = nothing
+    extra_force = nothing
+    p_extra = nothing
+end
+
+struct HydrodynamicSolution{TT,TX,TV}
     t::TT
     x::TX
     dx::TV
@@ -53,7 +65,7 @@ function calculate_excitation_force(current_time, excitation_coefficients, wave)
     ramp = ramp_function(start_time, ramp_time, current_time)
     exponential_term = omega_reshaped .* current_time .+ phase_reshaped
     force = ramp .* (excitation_coefficients[:, :, :, 1] .* cos.(exponential_term) -
-             excitation_coefficients[:, :, :, 2] .* sin.(exponential_term)) .*
+                     excitation_coefficients[:, :, :, 2] .* sin.(exponential_term)) .*
             sqrt.(2 * spectrum_reshaped .* frequency_spacing)
 
     # Format required for unitful input. `sum` doesn't play nice with matrices of mixed units and dimensions.
@@ -62,7 +74,7 @@ function calculate_excitation_force(current_time, excitation_coefficients, wave)
     return force[:, 1, :] * weights
 end
 
-function calculate_stiffness_force(position, stiffness; equilibrium_position = 0.0*position)
+function calculate_stiffness_force(position, stiffness; equilibrium_position=0.0*position)
     return -stiffness * (position - equilibrium_position)
 end
 
@@ -82,11 +94,11 @@ function calculate_radiation_force_convolution(velocity, irf)
 
     Kᵣ, tᵣ = irf # impulse response function tuple
 
-    integrand = sum(Kᵣ .* velocity_history; dims = [2])[:, 1, :] # nDOF, nDOF, nt --> nDOF, nt
-    dt = diff(tᵣ; dims = 3)[:, 1, :] # 1, nt-1
+    integrand = sum(Kᵣ .* velocity_history; dims=[2])[:, 1, :] # nDOF, nDOF, nt --> nDOF, nt
+    dt = diff(tᵣ; dims=3)[:, 1, :] # 1, nt-1
     radiation_force = sum(
-        (integrand[:, 1:(end - 1)] .+ integrand[:, 2:end]) .* 0.5 .* dt;
-        dims = [2])[:, 1] # nDOF
+        (integrand[:, 1:(end-1)] .+ integrand[:, 2:end]) .* 0.5 .* dt;
+        dims=[2])[:, 1] # nDOF
     return -radiation_force
 end
 
@@ -99,7 +111,7 @@ end
 function calculate_linear_force(velocity, position, coefficients)
     equilibrium_position, stiffness, damping = coefficients
     return calculate_damping_force(velocity, damping) +
-           calculate_stiffness_force(position, stiffness; equilibrium_position = equilibrium_position)
+           calculate_stiffness_force(position, stiffness; equilibrium_position=equilibrium_position)
 end
 
 function calculate_total_linear_hydro_forces(position, velocity, hydro, time)
@@ -111,76 +123,166 @@ function calculate_total_linear_hydro_forces(position, velocity, hydro, time)
 
     excitation_force = calculate_excitation_force(time, excitation_coefficients, wave)
     hydrostatic_stiffness_force = calculate_stiffness_force(
-        position, hydrostatic_stiffness_coefficient; equilibrium_position = 0.0*position)
+        position, hydrostatic_stiffness_coefficient; equilibrium_position=0.0*position)
     radiation_force = calculate_damping_force(velocity, radiation_damping_coefficient)
 
     return excitation_force .+ radiation_force .+ hydrostatic_stiffness_force .+
            net_gravity_buoyancy_force
 end
 
-function hydrodynamic_oscillator(u, p, t)
-    n_dof = Int64(length(u) / 2)
-    x = u[1:n_dof] # position
-    dx = u[(n_dof + 1):end] # velocity
+function hydrodynamic_oscillator(state, p::HydroParams, t)
+    n_dof = size(p.inverse_mass, 1)
+    n_hydro = 2 * n_dof
 
-    inverse_mass = p[1]
-    hydro = p[2]
-    force_other, u_other, p_other = p[3]
-    Fₜₒₜₐₗ = calculate_total_linear_hydro_forces(x, dx, hydro, t) +
-             force_other(t, [x; dx], u_other; p = p_other)
-    ddx = inverse_mass * Fₜₒₜₐₗ
+    hydro_state = state[1:n_hydro]
+    extra_state = state[(n_hydro+1):end]
 
-    return [dx; ddx]
+    x = hydro_state[1:n_dof]
+    dx = hydro_state[(n_dof+1):n_hydro]
+    platform_state = [x; dx]
+
+    Fₜₒₜₐₗ = calculate_total_linear_hydro_forces(x, dx, p.hydro, t) +
+             p.force_other(t, platform_state, p.u_control; p=p.p_other)
+
+    if p.extra_force !== nothing
+        Fₜₒₜₐₗ += p.extra_force(
+            t,
+            platform_state,
+            extra_state,
+            p.u_control;
+            p=p.p_extra,
+        )
+    end
+
+    ddx = p.inverse_mass * Fₜₒₜₐₗ
+    du_hydro = [dx; ddx]
+
+    if isempty(extra_state)
+        return du_hydro
+    end
+
+    p.extra_rhs === nothing &&
+        throw(ArgumentError("extra states were provided, but p.extra_rhs is nothing"))
+
+    du_extra = p.extra_rhs(
+        t,
+        platform_state,
+        extra_state,
+        p.u_control;
+        p=p.p_extra,
+    )
+
+    return [du_hydro; du_extra]
 end
 
-function hydrodynamic_oscillator_cic(u, p, t)
-    n_dof = Int64(length(u) / 2)
-    x = u[1:n_dof] # position
-    dx = u[(n_dof + 1):end] # velocity
+function hydrodynamic_oscillator_cic(state, p::HydroParams, t)
+    n_dof = size(p.inverse_mass, 1)
+    n_hydro = 2 * n_dof
 
-    inverse_mass = p[1]
-    hydro = p[2]
-    cic = hydro[6]
-    force_other, u_other, p_other = p[3]
-    Fₜₒₜₐₗ = calculate_total_linear_hydro_forces(x, dx, hydro, t) +
+    hydro_state = state[1:n_hydro]
+    extra_state = state[(n_hydro+1):end]
+
+    x = hydro_state[1:n_dof]
+    dx = hydro_state[(n_dof+1):n_hydro]
+    platform_state = [x; dx]
+
+    cic = p.hydro[6]
+
+    Fₜₒₜₐₗ = calculate_total_linear_hydro_forces(x, dx, p.hydro, t) +
              calculate_radiation_force_convolution(dx, cic) +
-             force_other(t, [x; dx], u_other; p = p_other)
-    ddx = inverse_mass * Fₜₒₜₐₗ
+             p.force_other(t, platform_state, p.u_control; p=p.p_other)
 
-    return [dx; ddx]
+    if p.extra_force !== nothing
+        Fₜₒₜₐₗ += p.extra_force(
+            t,
+            platform_state,
+            extra_state,
+            p.u_control;
+            p=p.p_extra,
+        )
+    end
+
+    ddx = p.inverse_mass * Fₜₒₜₐₗ
+    du_hydro = [dx; ddx]
+
+    if isempty(extra_state)
+        return du_hydro
+    end
+
+    p.extra_rhs === nothing &&
+        throw(ArgumentError("extra states were provided, but p.extra_rhs is nothing"))
+
+    du_extra = p.extra_rhs(
+        t,
+        platform_state,
+        extra_state,
+        p.u_control;
+        p=p.p_extra,
+    )
+
+    return [du_hydro; du_extra]
 end
 
-function hydrodynamic_oscillator_ss(u, p, t)
+function hydrodynamic_oscillator_ss(state, p::HydroParams, t)
     # u = [position, velocity, states]
     # added mass should utilize infinite frequency added mass only
     # c should not include radiation damping
     # system of equations in u and du should include velocity and the state space vector
-    inverse_mass = p[1]
-    hydro = p[2]
-    state_space = hydro[6]
-    force_other, u_other, p_other = p[3]
+    state_space = p.hydro[6]
     Aᵣ, Bᵣ, Cᵣ, Dᵣ, nₛₛ = state_space
-    n_dof = Int64((size(u)[1] - nₛₛ) / 2)
 
-    x = u[1:n_dof] # position
-    dx = u[(n_dof + 1):(n_dof * 2)] # velocity
-    ss = u[(end - nₛₛ + 1):end] # state space vector
+    n_dof = size(p.inverse_mass, 1)
+    n_hydro = 2 * n_dof + nₛₛ
 
+    hydro_state = state[1:n_hydro]
+    extra_state = state[(n_hydro+1):end]
+
+    x = hydro_state[1:n_dof]
+    dx = hydro_state[(n_dof+1):(2n_dof)]
+    ss = hydro_state[(2n_dof+1):(2n_dof+nₛₛ)]
+    platform_state = [x; dx]
     # The general state space is defined such that:
-    #    dx = Aᵣ * x + Bᵣ * u
-    #     y = Cᵣ * x + Dᵣ * u
+    #    dx = Aᵣ * x + Bᵣ * hydro_state
+    #     y = Cᵣ * x + Dᵣ * hydro_state
     # Where:
     #    x is the state vector (ss)
     #    y is the output (radiation force)
-    #    u is the input (velocity)
-    Fᵣ = - (Cᵣ * ss + Dᵣ * dx)
+    #    hydro_state is the input (velocity)
+    Fᵣ = -(Cᵣ * ss + Dᵣ * dx)
     dss = Aᵣ * ss + Bᵣ * dx
 
-    Fₜₒₜₐₗ = calculate_total_linear_hydro_forces(x, dx, hydro, t) + Fᵣ +
-             force_other(t, [x; dx], u_other; p = p_other)
-    ddx = inverse_mass * Fₜₒₜₐₗ
+    Fₜₒₜₐₗ = calculate_total_linear_hydro_forces(x, dx, p.hydro, t) + Fᵣ +
+             p.force_other(t, platform_state, p.u_control; p=p.p_other)
 
-    return [dx; ddx; dss]
+    if p.extra_force !== nothing
+        Fₜₒₜₐₗ += p.extra_force(
+            t,
+            platform_state,
+            extra_state,
+            p.u_control;
+            p=p.p_extra,
+        )
+    end
+
+    ddx = p.inverse_mass * Fₜₒₜₐₗ
+    du_hydro = [dx; ddx; dss]
+
+    if isempty(extra_state)
+        return du_hydro
+    end
+
+    p.extra_rhs === nothing &&
+        throw(ArgumentError("extra states were provided, but p.extra_rhs is nothing"))
+
+    du_extra = p.extra_rhs(
+        t,
+        platform_state,
+        extra_state,
+        p.u_control;
+        p=p.p_extra,
+    )
+
+    return [du_hydro; du_extra]
 end
 
 function hydrodynamic_stepping(dx0, x0, ts, p)
@@ -188,35 +290,39 @@ function hydrodynamic_stepping(dx0, x0, ts, p)
     x = [copy(x0) for _ in eachindex(ts)]
     dx = [copy(dx0) for _ in eachindex(ts)]
 
-    for i in 1:(length(ts) - 1)
-        dt = ts[i + 1] - ts[i]
-        acceleration = hydrodynamic_oscillator([x[i]; dx[i]], p, ts[i])
-        dx[i + 1] = dx[i] + dt * acceleration
-        x[i + 1] = x[i] + dt * dx[i + 1]
+    n_dof = size(p.inverse_mass, 1)
+
+    for i in 1:(length(ts)-1)
+        dt = ts[i+1] - ts[i]
+        du = hydrodynamic_oscillator([x[i]; dx[i]], p, ts[i])
+        ddx = du[(n_dof+1):(2n_dof)]
+
+        dx[i+1] = dx[i] + dt * ddx
+        x[i+1] = x[i] + dt * dx[i+1]
     end
 
     return HydrodynamicSolution(ts, x, dx)
 end
 
-function hydrodynamic_solver(u₀, ts, p; method::Symbol = :point)
-    # u₀ = [x₀, dx₀]
-    T = _real_eltype(u₀, p)
-    u₀ = T === eltype(u₀) ? u₀ : convert.(T, u₀)
+function hydrodynamic_solver(hydro_state₀, ts, p::HydroParams; method::Symbol=:point)
+    # hydro_state₀ = [x₀, dx₀]
+    T = _real_eltype(hydro_state₀, p)
+    hydro_state₀ = T === eltype(hydro_state₀) ? hydro_state₀ : convert.(T, hydro_state₀)
     dt = diff(ts[1:2])[1]
 
     if method == :point
-        problem = ODE.ODEProblem(hydrodynamic_oscillator, u₀, ts[[1, end]], p)
-        solution = ODE.solve(problem, ODE.Vern6(), saveat = dt)
+        problem = ODE.ODEProblem(hydrodynamic_oscillator, hydro_state₀, ts[[1, end]], p)
+        solution = ODE.solve(problem, ODE.Vern6(), saveat=dt)
 
     elseif method == :cic
-        init_velocity_history(T, size(p[2][6][1], 2), size(p[2][6][1], 3))
-        problem = ODE.ODEProblem(hydrodynamic_oscillator_cic, u₀, ts[[1, end]], p)
+        init_velocity_history(T, size(p.hydro[6][1], 2), size(p.hydro[6][1], 3))
+        problem = ODE.ODEProblem(hydrodynamic_oscillator_cic, hydro_state₀, ts[[1, end]], p)
         solution = ODE.solve(
-            problem, SDE.SimpleEuler(), saveat = dt, adaptive = false, dt = dt)
+            problem, SDE.SimpleEuler(), saveat=dt, adaptive=false, dt=dt)
 
     elseif method == :ss
-        problem = ODE.ODEProblem(hydrodynamic_oscillator_ss, u₀, ts[[1, end]], p)
-        solution = ODE.solve(problem, ODE.Vern6(), saveat = dt)
+        problem = ODE.ODEProblem(hydrodynamic_oscillator_ss, hydro_state₀, ts[[1, end]], p)
+        solution = ODE.solve(problem, ODE.Vern6(), saveat=dt)
     else
         throw(ArgumentError("method must be a Symbol with value :point, :cic, or :ss"))
     end
