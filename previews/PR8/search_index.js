1d366928f8001f0dc0e58a743ddb1e4e91e9d741
Documenter.jl <documenter@juliadocs.github.io>
2026-07-27 21:29:33 +0000

    delete history

---
+++ previews/PR8/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#Hydrodynamics.jl","page":"Home","text":"Tiny placeholder documentation for the RepoTemplate.jl package.","title":"Hydrodynamics.jl"},{"category":"section","location":"#Quick-Start","page":"Home","text":"using Hydrodynamics\n\ngreet()\ngreet(\"Julia\")","title":"Quick Start"},{"category":"section","location":"#API","page":"Home","text":"","title":"API"}]
+}
