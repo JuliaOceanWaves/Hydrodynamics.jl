6b2243311764d97637b619e2bb78907fcd988ca9
akeeste <akeeste@sandia.gov>
2026-07-27 18:02:13 -0500

    breakout regression tests into their own file

--- test/test_hydrodynamics.jl
+++ test/test_hydrodynamics.jl
@@ -1,8 +1,7 @@
+using DimensionfulAngles: radᵃ as rad
 using Hydrodynamics
 using Test
-using NBInclude
 using Unitful
-using DimensionfulAngles: radᵃ as rad
 
 @testset "ramp function" begin
     @test ramp_function(1.0, 3.0, 0.5) == 0.0
@@ -200,24 +199,4 @@ end
     @test length(hydro.period) == length(hydro.w)
     @test size(hydro.ex, 1) > 0
     @test size(hydro.khs, 1) > 0
-end
-
-@testset "WEC-Sim verification" begin
-    file = joinpath(@__DIR__, "..", "examples", "wec-sim_comparison_3dof.ipynb")
-    position_error = @nbinclude(file)
-    # accuracy of the verification to wec-sim when the notebook was first created. 
-    # last_result is the RMSE of position in [surge, heave, pitch] dofs x [cic, ss] methods
-    last_result = [0.0778 0.1116; 0.7228 0.7229; 0.0070 0.0074]
-    @test size(last_result) == size(position_error)
-    for idx in CartesianIndices(last_result)
-        @test position_error[idx] < last_result[idx]
-    end
-end
-
-@testset "AF verification" begin
-    # TODO - use power performance notebook to confirm gradients are functional
-    last_result = 0.051
-    file = joinpath(@__DIR__, "..", "examples", "power_performance.ipynb")
-    mae = @nbinclude(file)
-    @test mae < last_result
 end
